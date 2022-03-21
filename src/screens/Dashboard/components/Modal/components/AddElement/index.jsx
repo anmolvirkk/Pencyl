834a58c7767c73ec137d511346253783f94a35e8
Anmol Virk <theanmolvirk@gmail.com>
2022-03-22 01:59:02 +0530

    search icon

--- src/screens/Dashboard/components/Modal/components/AddElement/index.jsx
+++ src/screens/Dashboard/components/Modal/components/AddElement/index.jsx
@@ -1,15 +1,50 @@
 import { Search, Upload } from 'react-feather'
 import styles from './_addElement.module.sass'
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import { useRecoilState } from 'recoil'
 import elementsAtom from './elementsAtom'
 
 const Header = () => {
     const Searchbar = () => {
+
+        const query = useRef('')
+        
+        const [elements, setElements] = useRecoilState(elementsAtom)
+
+        const clientID = 58758127462563
+
+        const search = () => {
+
+            const getIcon = () => {
+                fetch(`https://api.iconscout.com/v3/search?query=${query.current}&product_type=item&asset=icon&per_page=10&page=1&sort=relevant&&price=free`, {
+                    headers: {
+                        'accept': 'application/json',
+                        'Client-ID': clientID
+                    }
+                }).then(res=>res.json()).then((res)=>{
+                    setElements({...elements, content: res.response.items.data})
+                })
+            }
+
+            switch (elements.type) {
+                case 'icons':
+                    getIcon()
+                break
+            }
+
+        }
+
+        const searchOnEnter = (e) => {
+            query.current = e.target.value
+            if(e.key.toLowerCase() === 'enter'){
+                search()
+            }
+        }
+        
         return (
             <div className={styles.search}>
-                <input type='text' placeholder='Search for any element' />
-                <Search />
+                <input type='text' placeholder='Search for any element' onKeyDown={(e)=>searchOnEnter(e)} />
+                <Search onMouseDown={search} />
             </div>
         )
     }
@@ -32,22 +67,7 @@ const Header = () => {
 
 const Assets = () => {
     
-    const [elements, setElements] = useRecoilState(elementsAtom)
-
-    const clientID = 58758127462563
-
-    const getIcon = (query) => {
-        fetch(`https://api.iconscout.com/v3/search?query=${query}&product_type=item&asset=icon&per_page=10&page=1&sort=relevant`, {
-            headers: {
-                'accept': 'application/json',
-                'Client-ID': clientID
-            }
-        }).then(res=>res.json()).then((res)=>{
-            setElements({...elements, content: res.response.items.data})
-        })
-    }
-
-    getIcon('apple')
+    const [elements] = useRecoilState(elementsAtom)
 
     return (
         <div className={styles.assets}>
@@ -55,6 +75,7 @@ const Assets = () => {
                 <div className={elements.type==='icons'?styles.active:null}>Icons</div>
                 <div className={elements.type==='illustrations'?styles.active:null}>Illustrations</div>
                 <div className={elements.type==='images'?styles.active:null}>Images</div>
+                <div className={elements.type==='uploaded'?styles.active:null}>Uploaded</div>
             </div>
             <div className={styles.content}>
                 {elements.content.map((item, key)=>{
