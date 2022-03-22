957adee5dc7a5ef1d7044f888b5bbf75bda331ab
Anmol Virk <theanmolvirk@gmail.com>
2022-03-23 02:56:11 +0530

    updated landing page

--- src/screens/LandingPage/components/Hero/index.jsx
+++ src/screens/LandingPage/components/Hero/index.jsx
@@ -1,4 +1,5 @@
 import styles from './_hero.module.sass'
+import { Link } from 'react-router-dom'
 
 const Hero = () => {
     return (
@@ -7,6 +8,7 @@ const Hero = () => {
                 <div className={styles.content}>
                     <h1>Create, generate and publish NFT collections from scratch</h1>
                     <p>Use powerful and intuitive tools for layering and editing your very own NFT projects.</p>
+                    <Link to='/dashboard'>Launch App</Link>
                 </div>
             </div>
             <video src='bg.mp4' autoPlay muted loop />
