2d9280ee1a50c378a591b4fb1850c8a49d0985e0
Anmol Virk <theanmolvirk@gmail.com>
2022-03-24 12:22:55 +0530

    edit details

--- src/screens/Dashboard/screens/Editor/components/Details/index.jsx
+++ src/screens/Dashboard/screens/Editor/components/Details/index.jsx
@@ -3,52 +3,40 @@ import layersAtom from '../Layers/layersAtom'
 import canvasAtom from './canvasAtom'
 import styles from './_details.module.sass'
 
-const Option = ({title, style}) => {
-
-    let value = style
-
-    const onBlur = () => {
-        // if(id){
-        //     if(value !== details.assets[id].style[title]){
-        //         let detailsString = JSON.stringify(details)
-        //         let newDetails = JSON.parse(detailsString)
-        //         newDetails.assets[id].style[title] = value
-        //         setDetails(newDetails)
-        //     }
-        // }else{
-        //     if(value !== style[title]){
-        //         let detailsString = JSON.stringify(details)
-        //         let newDetails = JSON.parse(detailsString)
-        //         newDetails.canvas.style[title] = value
-        //         setDetails(newDetails)
-        //     }
-        // }
-    }
+const Option = ({title, style, onBlur}) => {
 
     const preventEnter = (e) => {
         if(e.key === 'Enter'){
             e.preventDefault()
+            onBlur(title, e.target.innerText)
         }
     }
 
     return (
         <div className={styles.option}>
             <div className={styles.title}>{title}</div>
-            <div className={styles.input} contentEditable defaultValue='test' onKeyDown={(e)=>preventEnter(e)} dangerouslySetInnerHTML={{__html: value}} />
+            <div onBlur={(e)=>onBlur(title, e.target.innerText)} className={styles.input} contentEditable defaultValue='test' onKeyDown={(e)=>preventEnter(e)} dangerouslySetInnerHTML={{__html: style}} />
         </div>
     )
 }
 
 const Details = () => {
     const [canvas] = useRecoilState(canvasAtom)
-    const [layers] = useRecoilState(layersAtom)
+    const [layers, setLayers] = useRecoilState(layersAtom)
     let style = false
+    let onBlur = () => {}
     if(Object.keys(layers).length > 0){
         Object.keys(layers).forEach((item)=>{
             if(Object.keys(layers[item]).length > 0){
-                layers[item]['assets'].forEach((item)=>{
-                    if(item.active){
-                        style = item.style
+                layers[item]['assets'].forEach((item2, i)=>{
+                    if(item2.active){
+                        style = item2.style
+                        onBlur = (key, value) => {
+                            let layersString = JSON.stringify(layers)
+                            let newLayers = JSON.parse(layersString)
+                            newLayers[item]['assets'][i].style[key] = value
+                            setLayers({...newLayers})
+                        }
                     }
                 })
             }
@@ -57,11 +45,12 @@ const Details = () => {
     if(!style){
         style = canvas.style 
     }
+
     return (
         <div className={styles.details}>
             {
                 Object.keys(style).map((item, key)=>{
-                    return <Option key={key} title={item} style={style[item]} />
+                    return <Option key={key} title={item} style={style[item]} onBlur={onBlur} />
                 })
             }
         </div>
