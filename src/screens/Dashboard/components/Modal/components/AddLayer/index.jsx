2ef39f60e81c6c303b80ac21b5dd4a18560bfcdc
Anmol Virk <theanmolvirk@gmail.com>
2022-03-24 09:45:55 +0530

    add layer on enter

--- src/screens/Dashboard/components/Modal/components/AddLayer/index.jsx
+++ src/screens/Dashboard/components/Modal/components/AddLayer/index.jsx
@@ -3,9 +3,14 @@ import styles from './_addLayer.module.sass'
 
 const AddLayer = ({func}) => {
     const layerName = useRef('')
+    const onEnter = (e) => {
+        if(e.key === 'Enter'){
+            func(layerName.current)
+        }
+    }
     return (
         <div className={styles.wrapper}>
-            <input type='text' placeholder='Layer Name' onChange={(e)=>layerName.current=e.target.value} />
+            <input type='text' placeholder='Layer Name' onChange={(e)=>layerName.current=e.target.value} onKeyDown={(e)=>onEnter(e)} />
             <button onMouseDown={()=>func(layerName.current)}>
                 <p>Add</p>
             </button>
