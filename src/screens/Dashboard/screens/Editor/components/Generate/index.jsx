22d1241a29b4ba9042fba5f5b07e3b3741ef084d
Anmol Virk <theanmolvirk@gmail.com>
2022-05-06 21:44:13 +0530

    updating generate method

--- src/screens/Dashboard/screens/Editor/components/Generate/index.jsx
+++ src/screens/Dashboard/screens/Editor/components/Generate/index.jsx
@@ -10,39 +10,53 @@ const Images = () => {
   const [projects] = useRecoilState(projectsAtom)
   const [activeProject] = useRecoilState(activeProjectAtom)
   let currentProject = JSON.parse(projects.filter(i=>i.id===activeProject)[0].project)
+  let layerKeys = Object.keys(currentProject.layers)
+  layerKeys.reverse()
   useEffect(()=>{
     if(document.getElementById('images').children.length < parseInt(currentProject.supply)){
-      for(let i = 0; i<parseInt(currentProject.supply); i++){
+      const addImage = (res) => {
         setTimeout(()=>{
-          let image = []
-          let layerKeys = Object.keys(currentProject.layers)
-          layerKeys.reverse()
-          layerKeys.forEach((item)=>{
-            let random = Math.floor(Math.random() * (currentProject.layers[item].assets.length - 1))
-            image.push(currentProject.layers[item].assets[random].elem)
-          })
-          const html = ReactDOMServer.renderToStaticMarkup(
-            <div key={i} className={styles.imageWrapper} style={{aspectRatio: `${currentProject.canvas.width}/${currentProject.canvas.height}`}}>
-              <div className={styles.image} style={{background: currentProject.canvas.background}}>
-                  {image.map((item, key)=>{
-                    return (
-                      <div key={key} className={styles.asset}>
-                        <img src={item} alt='' />
-                      </div>
-                    )
-                  })}
+            let i = document.getElementById('images').children.length
+            let image = []
+            for(let i = 0; i < layerKeys.length; i++){
+              let random = Math.floor(Math.random() * (currentProject.layers[layerKeys[i]].assets.length - 1))
+              image.push(currentProject.layers[layerKeys[i]].assets[random].elem)
+            }
+            const html = ReactDOMServer.renderToStaticMarkup(
+              <div key={i} className={styles.imageWrapper} style={{aspectRatio: `${currentProject.canvas.width}/${currentProject.canvas.height}`}}>
+                <div className={styles.image} style={{background: currentProject.canvas.background}}>
+                    {image.map((item, key)=>{
+                      return (
+                        <div key={key} className={styles.asset}>
+                          <img src={item} alt='' />
+                        </div>
+                      )
+                    })}
+                </div>
               </div>
-            </div>
-          )
-          document.getElementById('images').innerHTML = document.getElementById('images').innerHTML+html
-          document.getElementById('imagesWrapper').scrollTo(0, document.getElementById('imagesWrapper').scrollHeight)
-          document.getElementById('currentImage').innerHTML = i+1
-          document.getElementById('totalImages').innerHTML = currentProject.supply
-          document.getElementById('progress').style.width = (((i+1)/parseInt(currentProject.supply))*100)+'%'
+            )
+            document.getElementById('images').innerHTML = document.getElementById('images').innerHTML+html
+            document.getElementById('imagesWrapper').scrollTo(0, document.getElementById('imagesWrapper').scrollHeight)
+            document.getElementById('currentImage').innerHTML = i+1
+            document.getElementById('totalImages').innerHTML = currentProject.supply
+            document.getElementById('progress').style.width = (((i+1)/parseInt(currentProject.supply))*100)+'%'
+            res(null)
         }, 0)
       }
+
+      const doNextPromise = (i) => {
+        Promise.resolve(new Promise(res=>addImage(res))).then(()=>{
+          i++
+          if (i < parseInt(currentProject.supply)){
+            doNextPromise(i)
+          }
+        })
+      }
+
+      doNextPromise(0)
     }
-  }, [currentProject])
+
+  }, [currentProject, layerKeys])
   return (
     <div className={styles.imagesWrapper} id='imagesWrapper'>
       <div className={styles.images} id='images' />
