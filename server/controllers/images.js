a779639a65742b66356ce4ba748bfd68b797756b
Anmol Virk <theanmolvirk@gmail.com>
2022-05-10 03:18:39 +0530

    open folder on windos

--- server/controllers/images.js
+++ server/controllers/images.js
@@ -1,5 +1,6 @@
 import os from 'os'
 import fs from 'fs'
+import { exec } from 'child_process'
 
 const decodeBase64Image = async (dataString) => {
     var matches = dataString.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/),
@@ -16,21 +17,32 @@ const decodeBase64Image = async (dataString) => {
 }
 
 const makeFolder = async (req) => {
-  let location = os.homedir()+'\\'+req.body.folder
-  const folder = location
+
+  let folder = os.homedir()+'\\'+req.body.folder.replace(' ', '_')
+
   if (!fs.existsSync(folder)) {
     fs.mkdirSync(folder)
   }
+
+  let openFolder = folder
+
+  if(os.userInfo().homedir.includes(' ')){
+    openFolder = folder.replace(os.userInfo().username, os.userInfo().username.toUpperCase().replace(' ', '').substring(0, 6)+'~1')
+  }
+
+  exec(`start ${openFolder}`, err=>{
+    console.log(err)
+  })
+
+  return folder
+
 }
 
 export const addImage = async (req, res) => {
     try {
-      let location = os.homedir()+'\\'+req.body.folder
-      const folder = location
-      makeFolder(req).then(()=>{
+      makeFolder(req).then((folder)=>{
         decodeBase64Image(req.body.image).then((e)=>{
           fs.readdir(folder, (_, files) => {
-            console.log(folder+'\\'+req.body.folder+'_#'+files.length+'.jpg')
               fs.writeFileSync(folder+'\\'+req.body.folder+'_#'+files.length+'.jpg', e.data, ()=>{console.log('image added')})
               res.json()
           })
