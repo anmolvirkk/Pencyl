1d5ada078f76b4cac8ac16100b11144b9580aa20
Anmol Virk <theanmolvirk@gmail.com>
2022-05-09 23:42:27 +0530

    sync online and download images

--- client/src/screens/Dashboard/screens/Editor/components/Generate/index.jsx
+++ client/src/screens/Dashboard/screens/Editor/components/Generate/index.jsx
@@ -8,47 +8,29 @@ import { FixedSizeGrid as Grid } from "react-window"
 import ReactDOMServer from 'react-dom/server'
 import { toJpeg } from 'dom-to-image'
 
-const Images = React.memo(() => {
+const Images = React.memo(({images}) => {
 
   const [projects] = useRecoilState(projectsAtom)
   const [activeProject] = useRecoilState(activeProjectAtom)
   let currentProject = JSON.parse(projects.filter(i=>i.id===activeProject)[0].project)
-  let layerKeys = Object.keys(currentProject.layers)
-  layerKeys.reverse()
-
-  const images = useRef({})
 
   const [resize, setResize] = useState(false)
   window.onresize = () => {
     setResize(!resize)
   }
 
-  const Image = React.memo(({style, columnIndex, rowIndex}) => {
-    let index = 'image'+parseInt(`${columnIndex}${rowIndex}`)
-    const randomNumbers = useRef([])
-    if(!images.current[index]){
-      let image = []
-      for(let i = 0; i < layerKeys.length; i++){
-        randomNumbers.current.push(Math.random())
-        const random = Math.floor(randomNumbers.current[i] * (currentProject.layers[layerKeys[i]].assets.length - 1))
-        image.push(currentProject.layers[layerKeys[i]].assets[random].elem)
-      }
-      images.current = {...images.current, [index]: image}
-      return (
-        <div className={styles.imageWrapper} style={{...style, aspectRatio: `${currentProject.canvas.width}/${currentProject.canvas.height}`}}>
-          <div className={styles.image} style={{background: currentProject.canvas.background}}>
-              {image.map((item, key)=>{
-                return (
-                  <div key={key} className={styles.asset}>
-                    <img src={item} alt='' />
-                  </div>
-                )
-              })}
-          </div>
-        </div>
-      )
-    }else{
-      let image = images.current[index]
+  const rowCount = Math.ceil(parseInt(currentProject.supply)/Math.floor(window.innerWidth / 300))
+  const columnCount = Math.floor(window.innerWidth / 300)
+
+  const gridIndex = useRef([])
+
+  const Image = React.memo(({style, rowIndex, columnIndex}) => {
+    let index = `${rowIndex}${columnIndex}`
+    if(!gridIndex.current.includes(index)){
+      gridIndex.current.push(index)
+    }
+    if(gridIndex.current.length <= images.current.length){
+      let image = images.current[gridIndex.current.indexOf(index)]
       return (
         <div style={{...style, aspectRatio: `${currentProject.canvas.width}/${currentProject.canvas.height}`}}>
           <div className={styles.image} style={{background: currentProject.canvas.background}}>
@@ -62,14 +44,16 @@ const Images = React.memo(() => {
           </div>
         </div>
       )
+    }else{
+      return null
     }
   })
 
   return (
     <div className={styles.imagesWrapper} id='imagesWrapper'>
       <Grid 
-        rowCount={Math.ceil(parseInt(currentProject.supply)/Math.floor(window.innerWidth / 300))}
-        columnCount={Math.floor(window.innerWidth / 300)}
+        rowCount={rowCount}
+        columnCount={columnCount}
         height={window.innerHeight - 120}
         width={window.innerWidth}
         rowHeight={300}
@@ -82,13 +66,11 @@ const Images = React.memo(() => {
 
 })
 
-const Footer = React.memo(() => {
+const Footer = React.memo(({images}) => {
 
   const [projects] = useRecoilState(projectsAtom)
   const [activeProject] = useRecoilState(activeProjectAtom)
   let currentProject = JSON.parse(projects.filter(i=>i.id===activeProject)[0].project)
-  let layerKeys = Object.keys(currentProject.layers)
-  layerKeys.reverse()
 
   useEffect(()=>{
     if(document.getElementById('total')){
@@ -101,46 +83,37 @@ const Footer = React.memo(() => {
       if(i < parseInt(currentProject.supply)){
           document.getElementById('current').innerHTML = i + 1
           document.getElementById('progressIndicator').style.width = (((i + 1)/parseInt(currentProject.supply))*100)+'%'
-          let image = []
-          const addLayer = (j) => {
-              if(j < layerKeys.length){
-                const random = Math.floor(Math.random() * (currentProject.layers[layerKeys[j]].assets.length - 1))
-                image.push(currentProject.layers[layerKeys[j]].assets[random].elem)
-                addLayer(j + 1)
-              }else{
-                  let div = document.createElement('div')
-                  let targetHTML = ReactDOMServer.renderToStaticMarkup(
-                    <div style={{background: currentProject.canvas.background, position: 'relative', height: '784px', width: '784px'}}>
-                        {image.map((item, key)=>{
-                          return (
-                            <div key={key} style={{position: 'absolute', width: '100%'}}>
-                              <img src={item} alt='' style={{width: '100%'}} />
-                            </div>
-                          )
-                        })}
+          let image = images.current[i]
+          let div = document.createElement('div')
+          let targetHTML = ReactDOMServer.renderToStaticMarkup(
+            <div style={{background: currentProject.canvas.background, position: 'relative', height: '784px', width: '784px'}}>
+                {image.map((item, key)=>{
+                  return (
+                    <div key={key} style={{position: 'absolute', width: '100%'}}>
+                      <img src={item} alt='' style={{width: '100%'}} />
                     </div>
                   )
-                  div.innerHTML = targetHTML
-                  document.getElementById('download').append(div)
-                  setTimeout(()=>{
-                    toJpeg(div.childNodes[0]).then((e)=>{
-                      if(e !== 'data:,'){
-                        fetch('http://localhost:5000/image', {
-                            method: 'POST',
-                            headers: {
-                                'Content-Type': 'application/json'
-                            },
-                            body: JSON.stringify({folder: currentProject.name, image: e})
-                        }).then(()=>{
-                          document.getElementById('download').innerHTML = ''
-                          supplyImage(i+1)
-                        })
-                      }
-                    })
-                  }, 1000)
+                })}
+            </div>
+          )
+          div.innerHTML = targetHTML
+          document.getElementById('download').append(div)
+          setTimeout(()=>{
+            toJpeg(div.childNodes[0]).then((e)=>{
+              if(e !== 'data:,'){
+                fetch('http://localhost:5000/image', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({folder: currentProject.name, image: e})
+                }).then(()=>{
+                  document.getElementById('download').innerHTML = ''
+                  supplyImage(i+1)
+                })
               }
-          }
-          addLayer(0)
+            })
+          }, 1000)
       }
     }
     supplyImage(0)
@@ -166,11 +139,29 @@ const Footer = React.memo(() => {
 })
 
 const Generate = () => {
+
+  const [projects] = useRecoilState(projectsAtom)
+  const [activeProject] = useRecoilState(activeProjectAtom)
+  let currentProject = JSON.parse(projects.filter(i=>i.id===activeProject)[0].project)
+  let layerKeys = Object.keys(currentProject.layers)
+  layerKeys.reverse()
+
+  const images = useRef([])
+
+  for(let i = 0; i < parseInt(currentProject.supply); i++){
+    let image = []
+    for(let i = 0; i < layerKeys.length; i++){
+      const random = Math.floor(Math.random() * (currentProject.layers[layerKeys[i]].assets.length - 1))
+      image.push(currentProject.layers[layerKeys[i]].assets[random].elem)
+    }
+    images.current.push(image)
+  }
+
   return (
     <div className={styles.generate}>
         <Header type='generate' />
-        <Images />
-        <Footer />
+        <Images images={images} />
+        <Footer images={images} />
         <div id='download' className={styles.download} />
     </div>
   )
