5db48bbd9cdd7542c793d985919e4dc9717f737f
Anmol Virk <theanmolvirk@gmail.com>
2022-05-10 02:11:29 +0530

    update server

--- server/controllers/images.js
+++ server/controllers/images.js
@@ -15,25 +15,27 @@ const decodeBase64Image = async (dataString) => {
     return response
 }
 
+const makeFolder = async (req) => {
+  let location = os.homedir()+'\\'+req.body.folder
+  const folder = location
+  if (!fs.existsSync(folder)) {
+    fs.mkdirSync(folder)
+  }
+}
+
 export const addImage = async (req, res) => {
     try {
-        const folder = os.homedir()+'\\'+req.body.folder
-        if (!fs.existsSync(folder)) {
-          fs.mkdirSync(folder)
-          await decodeBase64Image(req.body.image).then((e)=>{
-            fs.readdir(folder, (_, files) => {
-                fs.writeFile(folder+'\\'+req.body.folder+'_#'+files.length+'.jpg', e.data, ()=>{console.log('image added')})
-                res.json()
-            })
+      let location = os.homedir()+'\\'+req.body.folder
+      const folder = location
+      makeFolder(req).then(()=>{
+        decodeBase64Image(req.body.image).then((e)=>{
+          fs.readdir(folder, (_, files) => {
+            console.log(folder+'\\'+req.body.folder+'_#'+files.length+'.jpg')
+              fs.writeFileSync(folder+'\\'+req.body.folder+'_#'+files.length+'.jpg', e.data, ()=>{console.log('image added')})
+              res.json()
           })
-        }else{
-          await decodeBase64Image(req.body.image).then((e)=>{
-            fs.readdir(folder, (_, files) => {
-                fs.writeFile(folder+'\\'+req.body.folder+'_#'+files.length+'.jpg', e.data, ()=>{console.log('image added')})
-                res.json()
-            })
-          })
-        }
+        })
+      })
     } catch (error) {
         res.json({message: error.message})
     }
